e3b2fa4921b0882dd540a725fe209244e6641b04
Zamoula <jamelmrad000@gmail.com>
2025-12-20 16:55:07 +0100

    register ui impl

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { Password, PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        CardModule,
+        PasswordModule,
+        ButtonModule,
+        InputTextModule,
+        FloatLabelModule,
+        CommonModule,
+        RouterModule,
+        DividerModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  confirmPassword: any = '';
+  user: any = {
+    username: '',
+    email: '',
+    password: ''
+  }
+
+  register(user: any) {
+    console.log(user);
+    
+  }
 
 }
