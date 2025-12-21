91b3359f27a6953829b6004d84d42a7bc4697155
Zamoula <jamelmrad000@gmail.com>
2025-12-21 13:59:39 +0100

    authentication interceptor impl

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,15 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideRouter(routes),
     provideAnimationsAsync(),
         providePrimeNG({ 
